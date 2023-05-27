70aebb5cc98a886773c01301f7ad1656a0ba21b5
Gustavo Caso <gustavocaso@gmail.com>
2023-05-27 20:58:48 +0200

    update server to parse get, set and del commands

--- src/server.cpp
+++ src/server.cpp
@@ -12,6 +12,7 @@
 #include <fcntl.h>
 #include <string>
 #include <vector>
+#include <map>
 #include "helper.hpp"
 
 enum
@@ -33,6 +34,47 @@ struct Conn
   size_t wbuf_sent = 0;
   uint8_t wbuf[4 + K_MAX_MSG];
 };
+
+enum
+{
+  RES_OK = 0,
+  RES_ERR = 1,
+  RES_NX = 2,
+};
+
+// The data structure for the key space. Ths is just a placeholder
+// until we implement a hashtable
+static std::map<std::string, std::string> g_map;
+
+static uint32_t do_get(const std::vector<std::string> &cmd, uint8_t *res, uint32_t *reslen)
+{
+  if (!g_map.count(cmd[1]))
+  {
+    return RES_NX;
+  }
+
+  std::string &val = g_map[cmd[1]];
+  assert(val.size() <= K_MAX_MSG);
+  memcpy(res, val.data(), val.size());
+  *reslen = (uint32_t)val.size();
+  return RES_OK;
+}
+
+static uint32_t do_set(const std::vector<std::string> &cmd, uint8_t *res, uint32_t *reslen)
+{
+  (void)res;
+  (void)reslen;
+  g_map[cmd[1]] = cmd[2];
+  return RES_OK;
+}
+
+static uint32_t do_del(const std::vector<std::string> &cmd, uint8_t *res, uint32_t *reslen)
+{
+  (void)res;
+  (void)reslen;
+  g_map.erase(cmd[1]);
+  return RES_OK;
+}
 
 static void fd_set_nb(int fd)
 {
@@ -137,6 +179,84 @@ static void state_res(Conn *conn)
   }
 }
 
+static int32_t parse_req(const uint8_t *data, size_t len, std::vector<std::string> &out)
+{
+  if (len < 4)
+  {
+    return -1;
+  }
+
+  uint32_t n = 0;
+  memcpy(&n, &data[0], 4);
+  if (n > K_MAX_MSG)
+  {
+    return -1;
+  }
+
+  size_t pos = 4;
+  while (n--)
+  {
+    if (pos + 4 > len)
+    {
+      return -1;
+    }
+    uint32_t sz = 0;
+    memcpy(&sz, &data[pos], 4);
+    if (pos + 4 + sz > len)
+    {
+      return -1;
+    }
+    out.push_back(std::string((char *)&data[pos + 4], sz));
+    pos += 4 + sz;
+  }
+
+  if (pos != len)
+  {
+    return -1;
+  }
+
+  return 0;
+}
+
+static bool cmd_is(const std::string &word, const char *cmd)
+{
+  return 0 == strcasecmp(word.c_str(), cmd);
+}
+
+static int32_t do_request(const uint8_t *req, uint32_t reqlen, uint32_t *rescode, uint8_t *res, uint32_t *reslen)
+{
+  std::vector<std::string> cmd;
+
+  if (0 != parse_req(req, reqlen, cmd))
+  {
+    msg("bad req");
+    return -1;
+  }
+
+  if (cmd.size() == 2 && cmd_is(cmd[0], "get"))
+  {
+    *rescode = do_get(cmd, res, reslen);
+  }
+  else if (cmd.size() == 3 && cmd_is(cmd[0], "set"))
+  {
+    *rescode = do_set(cmd, res, reslen);
+  }
+  else if (cmd.size() == 2 && cmd_is(cmd[0], "del"))
+  {
+    *rescode = do_del(cmd, res, reslen);
+  }
+  else
+  {
+    // cmd is not recognized
+    *rescode = RES_ERR;
+    const char *msg = "Unknown cmd";
+    strcpy((char *)res, msg);
+    *reslen = strlen(msg);
+    return 0;
+  }
+  return 0;
+}
+
 static bool try_one_request(Conn *conn)
 {
   // try to parse a request from the body
@@ -161,12 +281,20 @@ static bool try_one_request(Conn *conn)
   }
 
   // got one request, do something with it
-  printf("client says: %.*s\n", len, &conn->rbuf[4]);
+  uint32_t rescode = 0;
+  uint32_t wlen = 0;
+  int32_t err = do_request(&conn->rbuf[4], len, &rescode, &conn->wbuf[4 + 4], &wlen);
 
-  // generating echoing response
-  memcpy(&conn->wbuf[0], &len, 4);
-  memcpy(&conn->wbuf[4], &conn->rbuf[4], len);
-  conn->wbuf_size = 4 + len;
+  if (err)
+  {
+    conn->state = STATE_END;
+    return false;
+  }
+
+  wlen += 4;
+  memcpy(&conn->wbuf[0], &wlen, 4);
+  memcpy(&conn->wbuf[4], &rescode, 4);
+  conn->wbuf_size = 4 + wlen;
 
   // remove the request from the buffer.
   // note: frequent memmove is ineficient.
